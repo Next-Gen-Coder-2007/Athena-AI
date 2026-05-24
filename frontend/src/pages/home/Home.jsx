dac26354d8dfd639b60d681395cb64e0f9b7e7ab
Subash B <subash59245@gmail.com>
2026-05-24 13:51:31 +0530

    feat: initialize frontend and backend with successful API testing

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,26 @@
+import './Home.css'
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
+
+const Home = () => {
+    const [message, setMessage] = useState()
+
+    useEffect(() => {
+        fetchMessage();
+    }, [])
+
+    const fetchMessage = async () => {
+        try {
+            const res = await api.get('/')
+            setMessage(res.data.message)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    return (
+        <div>{message}</div>
+    )
+}
+
+export default Home
